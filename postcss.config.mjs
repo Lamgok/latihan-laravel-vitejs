74bc004e268e6672e0168f57808d7a66e466ebde
Lamgok <andosiahaan13@gmail.com>
2025-11-12 14:47:23 +0700

    push

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
-// auxtern/latihan-laravel-vitejs/latihan-laravel-vitejs-63c082aeefce030e1f084888e73d3ebd6a1c4f07/postcss.config.mjs
 export default {
     plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
+        "@tailwindcss/postcss": {},
+        "tailwindcss/nesting": {}, // Tambahkan jika menggunakan fitur nesting
+        "@tailwindcss/typography": {}, // <-- BARIS BARU: Tambahkan plugin Typography
     },
 };
